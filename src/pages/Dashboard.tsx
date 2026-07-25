7c76b950f77f09e2e50d2d4678fbe39b141c29e3
usaidm291 <usaidm291@gmail.com>
2026-07-25 19:39:05 +0530

    Add files via upload

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -166,7 +166,7 @@ export function Dashboard() {
                     );
                   })
                 )
-                )}
+                }
               </div>
             </div>
             {/* Stats */}
